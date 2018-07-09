974a7f30981aa62cfbfae3ab8a50c9c4f21f718a
Robert Davis <robert.davis@progleasing.com>
2018-07-09 16:41:10 -0600

    Add some helper functions

---
+++ SQLHelper/SQLHelper/ProcHelper/Functions/Private_CreateRevisionLine.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE   FUNCTION ProcHelper.Private_CreateRevisionLine(@Ticket NVARCHAR(10), @Comment NVARCHAR(50) = 'Initial creation')
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @RevisionLine NVARCHAR(MAX)
+        ,@Date NVARCHAR(10) = CONVERT(VARCHAR(MAX),GETDATE(),101);
+    SET @RevisionLine = @Date
+                        +ProcHelper.Private_CreateStringOfLength(16-LEN(@Date),' ')
+                        +SUSER_NAME()
+                        +ProcHelper.Private_CreateStringOfLength(28-LEN(SUSER_NAME()),' ')
+                        +@Ticket
+                        +ProcHelper.Private_CreateStringOfLength(16-LEN(@Ticket),' ')
+                        +@Comment
+    RETURN @RevisionLine;
+END;
---
+++ SQLHelper/SQLHelper/ProcHelper/Functions/Private_CreateStringOfLength.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE   FUNCTION ProcHelper.Private_CreateStringOfLength(@Length INT,@Char CHAR)
+RETURNS NVARCHAR(MAX)
+AS 
+BEGIN
+    DECLARE 
+        @String NVARCHAR(MAX) = ''
+        ,@CurrentCount INT = 0;
+    WHILE @CurrentCount < @Length
+    BEGIN
+        SET @String += @Char;
+        SET @CurrentCount += 1;
+    END;
+    RETURN @String;
+END;
